9850d43acf4d0d7deff21514ba123b334f5b7f25
michionle <briantadr24@gmail.com>
2024-06-29 16:17:43 +0700

    UI: Fix Header

--- admin/pages/kategori/kategori.php
+++ admin/pages/kategori/kategori.php
@@ -32,36 +32,27 @@ if (!isset($_SESSION['username'])) {
   <div class="container-scroller">
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo" href="index.php"><img src="../../images/Markiba.png" alt="logo" style="width: 50px; height: 50px; margin-right:20px"/><span class="text-primary" style="font-weight: bold">MARKIBA<span></a>
-        <a class="navbar-brand brand-logo-mini" href="index.php"><img src="../../images/Markiba.png" alt="logo" style="width: 50px; height: 50px; margin-left: 20px"/></a>
-      </div>
-      <div class="navbar-menu-wrapper d-flex align-items-stretch">
-        <ul class="navbar-nav navbar-nav-right">
-          <li class="nav-item nav-profile dropdown">
-            <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-              <div class="nav-profile-img">
-                <i class="mdi mdi-account-circle"></i>
-                <span class="availability-status online"></span>             
-              </div>
-              <div class="nav-profile-text">
-                <p class="mb-1 text-black">Admin</p>
-              </div>
-            </a>
-            <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">
-                <i class="mdi mdi-logout mr-2 text-primary"></i>
-                Signout
-              </a>
+            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+                <a class="navbar-brand brand-logo" href="index.php"><img src="../../images/Markiba.png" alt="logo"
+                        style="width: 50px; height: 50px; margin-right:20px" /><span class="text-primary"
+                        style="font-weight: bold">MARKIBA<span></a>
+                <a class="navbar-brand brand-logo-mini" href="index.php"><img src="../../images/Markiba.png"
+                        alt="logo" style="width: 50px; height: 50px; margin-left: 20px" /></a>
             </div>
-          </li>
-        </ul>
-        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-          <span class="mdi mdi-menu"></span>
-        </button>
-      </div>
-    </nav>
+            <div class="navbar-menu-wrapper d-flex align-items-stretch">
+                <ul class="navbar-nav navbar-nav-right">
+                    <li class="nav-item nav-profile dropdown" style="display: flex; align-items: center;">
+                        <i class="mdi mdi-account-circle"
+                            style="font-size: 18pt; margin-right: 8px; color: #B66DFF"></i>
+                        <span style="color: #B66DFF"><?php echo $_SESSION['nama']; ?></span>
+                    </li>
+                </ul>
+                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
+                    data-toggle="offcanvas">
+                    <span class="mdi mdi-menu"></span>
+                </button>
+            </div>
+        </nav>
     <!-- partial -->
     <div class="container-fluid page-body-wrapper">
     <?php
