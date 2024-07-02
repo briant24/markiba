7b1403f4233c0f43d3b5e325153c11f2f8efd4d7
michionle <briantadr24@gmail.com>
2024-07-02 17:13:50 +0700

    DB: Remove Ip Address from history

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
 <?php
 include 'koneksi.php';
-
-function get_client_ip() {
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if(isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-}
-
-$ip_address = get_client_ip();
-
-$query = "INSERT INTO pengunjung (ip_address) VALUES ('$ip_address')";
-
+$query = "INSERT INTO pengunjung (waktu) VALUES (CURRENT_TIMESTAMP)";
 mysqli_query($conn, $query);
 mysqli_close($conn);
 ?>
