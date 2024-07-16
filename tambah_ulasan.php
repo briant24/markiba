b7c4304f19620d6678026169e4388caa2ef1c5d3
michionle <briantadr24@gmail.com>
2024-07-16 23:48:01 +0700

    UI: Implement Star UI [User Side]

--- tambah_ulasan.php
+++ tambah_ulasan.php
@@ -24,8 +24,29 @@ if (!isset($_SESSION['username'])) {
   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
   <link rel="shortcut icon" href="assets/images/Markiba.png" />
   <style>
-    
-  </style>
+    .star-rating {
+        direction: rtl; /* Right-to-left to show stars from right to left */
+        display: table;
+        font-size: 1.5em;
+        margin-top: -10px;
+    }
+    .star-rating input[type="radio"] {
+        display: none; /* Hide the actual radio buttons */
+    }
+    .star-rating label {
+        color: #ddd; /* Default color of stars */
+        font-size: 2em;
+        cursor: pointer;
+    }
+    .star-rating input[type="radio"]:checked ~ label {
+        color: #f5c518; /* Color of stars when checked */
+    }
+    .star-rating label:hover,
+    .star-rating label:hover ~ label {
+        color: #f5c518; /* Color of stars on hover */
+    }
+</style>
+
 </head>
 <?php
 $activePage = 'ulasan';
@@ -54,8 +75,14 @@ $id_buku = $_GET['id_buku'];
                         <input type="hidden" name="id_buku" value="<?php echo $id_buku; ?>">
                         <!-- New field for adding review -->
                         <div class="form-group">
-                            <label for="rating">Rating</label>
-                            <input type="number" step="1" class="form-control" id="rating" name="rating" placeholder="Rating (0-5)" min="0" max="5" required>
+                          <label for="rating">Rating</label>
+                          <div class="star-rating">
+                              <input type="radio" id="star5" name="rating" value="5" required /><label for="star5" title="5 stars">☆</label>
+                              <input type="radio" id="star4" name="rating" value="4" required /><label for="star4" title="4 stars">☆</label>
+                              <input type="radio" id="star3" name="rating" value="3" required /><label for="star3" title="3 stars">☆</label>
+                              <input type="radio" id="star2" name="rating" value="2" required /><label for="star2" title="2 stars">☆</label>
+                              <input type="radio" id="star1" name="rating" value="1" required /><label for="star1" title="1 star">☆</label>
+                          </div>
                         </div>
                         <div class="form-group">
                             <label for="isi_ulasan">Ulasan</label>
